0ca7b888506169fb79eb0c29bc0dab5694100249
leaf <zhangqp5@mail3.sysu.edu.cn>
2023-01-18 17:07:52 +0800

    添加立体字体页面

--- src/pages/threeDimensional/index.tsx
+++ src/pages/threeDimensional/index.tsx
@@ -4,7 +4,7 @@ import './index.less';
 const ThreeDimensional: React.FC = () => {
   return (
     <div className="three-dimensional-container">
-      <h1>1</h1>
+      <h1>派大星&海绵宝宝</h1>
     </div>
   )
 }
